9213a5df8b16f9b7b35bcd1e998285d21b4b17f7
Samuel Bosawer <zam.bosawer@gmail.com>
2023-05-30 21:06:40 +0900

    ➕ CDN Taildwindcss

--- resources/views/visitor/includes/header.blade.php
+++ resources/views/visitor/includes/header.blade.php
@@ -15,7 +15,7 @@
 
 
     <!-- TAILWIND CSS -->
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- FLOWBITE -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
