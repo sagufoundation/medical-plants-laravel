c04d82d00476a90234d6334f512dbd9462dfbfd2
Samuel Bosawer <zam.bosawer@gmail.com>
2023-08-15 06:29:54 +0900

    Detail Plant Done & ⏳Edit Plant

--- resources/views/admin/pages/plant/detail.blade.php
+++ resources/views/admin/pages/plant/detail.blade.php
@@ -24,6 +24,33 @@
               <div class="card-body">
                 <h5 class="card-title mb-0">Plants</h5>
 
+                <table>
+                    <tr>
+                        <td width="300"> Local Name </td>
+                        <th>   {{ $data->local_name }} </th>
+                    </tr>
+                    <tr>
+                        <td width="300"> Taxonomists </td>
+                        <th>    {{ $data->taxonomists }} </th>
+                    </tr>
+                    <tr>
+                        <td width="300"> Treatments </td>
+                        <th>   {{ $data->treatments }} </th>
+                    </tr>
+                    <tr>
+                        <td width="300"> Status </td>
+                        <th>    {{$data->status}} </th>
+                    </tr>
+                    <tr>
+                        <td width="300">  Picture Cover </td>
+                        <th>   <img  alt=" Picture Cover" src="{{ url($data->cover_picture) }}" width="300"> </th>
+                    </tr>
+                    <tr>
+                        <td width="300"> Gallery Picture </td>
+                        <th>   <img  alt="Gallery Picture" src="{{ url($data->gallery_picture) }}" width="300"> </th>
+                    </tr>
+                </table>
+
             </div>
 
           </div>
