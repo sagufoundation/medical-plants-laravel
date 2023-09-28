655d0e88b5ba22a3d61ba451fc531e809d3c8c76
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-09-28 17:15:51 +0900

    UPDATED: our sponsor page

--- resources/views/visitor/pages/our-sponsors.blade.php
+++ resources/views/visitor/pages/our-sponsors.blade.php
@@ -6,12 +6,19 @@
 
 <section id="the-plants ">
     <div class="container mt-5 mb-5">
-    <div class="row">
-        <div class="col-lg-6 mx-auto text-center">
-            <h1 class="fw-bolder font-satu text-success">Our Sponsors</h1>
-            <p>Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+        <div class="row">
+            <div class="col-lg-6 mx-auto text-center">
+                <h1 class="fw-bolder font-satu text-success">Our Sponsors</h1>
+                <p>Discover the traditional medicinal plants recognized by Indigenous Papuans in Papua, Indonesia. Our comprehensive database includes information on their traditional uses, chemical properties, and potential health benefits. </p>
+            </div>
         </div>
-    </div>
+
+        <div class="row">
+            <div class="col-lg-6 mx-auto text-center">
+                <img src="{{ asset('images/illustrations/contributor1.png') }}" alt="Illustration image" class="w-75">
+            </div>
+        </div>
+
     </div>
 </section>
 
