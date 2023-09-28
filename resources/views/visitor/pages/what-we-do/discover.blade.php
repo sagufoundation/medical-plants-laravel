bcd285b4ff3aa268f692d73e575f5f72a3ff4c6b
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-09-28 16:57:29 +0900

    UPDATED: discover, research

--- resources/views/visitor/pages/what-we-do/discover.blade.php
+++ resources/views/visitor/pages/what-we-do/discover.blade.php
@@ -6,12 +6,18 @@
 
 <section id="the-plants ">
     <div class="container mt-5 mb-5">
-    <div class="row">
-        <div class="col-lg-6 mx-auto text-center">
-            <h1 class="fw-bolder font-satu text-success">Discover</h1>
-            <p>This part of the database allows users to explore the traditional medicinal plants of Papua based on their scientific names, common names, or medicinal uses. Users can also browse through images of the plants and learn about their cultural significance and traditional use.</p>
+        <div class="row">
+            <div class="col-lg-6 mx-auto text-center">
+                <h1 class="fw-bolder font-satu text-success">Discover</h1>
+                <p>This part of the database allows users to explore the traditional medicinal plants of Papua based on their scientific names, common names, or medicinal uses. Users can also browse through images of the plants and learn about their cultural significance and traditional use.</p>
+            </div>
         </div>
-    </div>
+
+        <div class="row">
+            <div class="col-lg-6 mx-auto text-center p-5">
+                <img src="{{ asset('images/illustrations/discover.png') }}" alt="Illustration image" class="w-50">
+            </div>
+        </div>
     </div>
 </section>
 
--- resources/views/visitor/pages/what-we-do/research.blade.php
+++ resources/views/visitor/pages/what-we-do/research.blade.php
@@ -6,12 +6,19 @@
 
 <section id="the-plants ">
     <div class="container mt-5 mb-5">
-    <div class="row">
-        <div class="col-lg-6 mx-auto text-center">
-            <h1 class="fw-bolder font-satu text-success">Research</h1>
-            <p>This part of the database provides researchers with access to information on the chemical properties of the plants, their potential therapeutic effects, and their traditional use. Researchers can also contribute to the database by sharing their own research findings and publications on traditional medicinal plants from Papua.</p>
+        <div class="row">
+            <div class="col-lg-6 mx-auto text-center">
+                <h1 class="fw-bolder font-satu text-success">Research</h1>
+                <p>This part of the database provides researchers with access to information on the chemical properties of the plants, their potential therapeutic effects, and their traditional use. Researchers can also contribute to the database by sharing their own research findings and publications on traditional medicinal plants from Papua.</p>
+            </div>
         </div>
-    </div>
+
+        <div class="row">
+            <div class="col-lg-6 mx-auto text-center p-5">
+                <img src="{{ asset('images/illustrations/research.png') }}" alt="Illustration image" class="w-50">
+            </div>
+        </div>
+
     </div>
 </section>
 
