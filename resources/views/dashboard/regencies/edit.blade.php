96e92abc063e921be54193a636108f91a7ffbddc
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-04-10 19:15:08 +0900

    fix: regencycontroller - updating view edit page cols and rows

--- resources/views/dashboard/regencies/edit.blade.php
+++ resources/views/dashboard/regencies/edit.blade.php
@@ -15,8 +15,11 @@
                 @csrf
                 @method('put')
 
+                    <!-- row start -->
                     <div class="row">
-                        <div class="col-lg-6">
+                        
+                        <!-- .col start -->
+                        <div class="col-lg-8">
 
                             <!-- input item START -->
                             <div class="mb-3">
@@ -73,16 +76,40 @@
                                     </span>
                                 @endif
                             </div>
-                            <!-- input item END -->                            
+                            <!-- input item END -->
+
+                            <!-- input item START -->
+                            <div class="mb-3">
+                                <label for="description">Status</label>
+                                <select name="status" class="form-control" id="">
+                                    {{-- <option value="Draft" value="" hidden>Select</option> --}}
+                                    <option value="Publish" @if($data->status == 'Publish') selected @endif>Publish</option>
+                                    <option value="Draft" @if($data->status == 'Draft') selected @endif>Draft</option>
+                                </select>
+
+                                @if ($errors->has('status'))
+                                <span class="text-danger" role="alert">
+                                        <small>{{ $errors->first('status') }}</small>
+                                    </span>
+                                @endif
+
+                            </div>
+                            <!-- input item END -->
+
+                        </div>
+                        <!-- .col end -->
+
+                        <!-- .col start -->
+                        <div class="col-lg-4">                            
 
                             <!-- input item START -->
                             <div class="mb-3">
                                 <label for="image" class="form-label d-block">Image / Logo</label>
                                 <div class="mb-2">
                                     @if($data->image)
-                                        <img src="{{ asset('images/regencies/' . $data->image) }}" alt="Gambar" id="preview-gambar" class="img-thumbnail w-50">
+                                        <img src="{{ asset('images/regencies/' . $data->image) }}" alt="Gambar" id="preview-gambar" class="img-thumbnail w-100">
                                     @else 
-                                        <img src="{{ asset('images/00.png') }}" alt="Gambar" id="preview-gambar" class="img-thumbnail w-50">
+                                        <img src="{{ asset('images/00.png') }}" alt="Gambar" id="preview-gambar" class="img-thumbnail w-100">
                                     @endif
                                 </div>
 
@@ -105,35 +132,20 @@
                             </div>
                             <!-- input item END -->
 
-                            <!-- input item START -->
-                            <div class="mb-3">
-                                <label for="description">Status</label>
-                                <select name="status" class="form-control" id="">
-                                    {{-- <option value="Draft" value="" hidden>Select</option> --}}
-                                    <option value="Publish" @if($data->status == 'Publish') selected @endif>Publish</option>
-                                    <option value="Draft" @if($data->status == 'Draft') selected @endif>Draft</option>
-                                </select>
-
-                                @if ($errors->has('status'))
-                                <span class="text-danger" role="alert">
-                                        <small>{{ $errors->first('status') }}</small>
-                                    </span>
-                                @endif
-
-                            </div>
-                            <!-- input item END -->
-
                         </div>
+                        <!-- .col end -->
 
                     </div>
+                    <!-- .row end -->
 
-                    <button type="submit" class="btn btn-primary rounded-0">
-                        <i class="fa-solid fa-save"></i> Save
-                    </button>
-
-                    <a href="{{ route(Request::segment(1).'.'.Request::segment(2).'') }}" class="btn btn-outline-dark rounded-0 border-0">
-                        <i class="fa-solid fa-times-square"></i> Cancle
-                    </a>
+                    <!-- row start -->
+                    <div class="row">
+                        <div class="col">
+                            <x-submit-button />
+                            <x-close-button/>
+                        </div>
+                    </div> 
+                    <!-- .row end --> 
 
                 </form>
 
@@ -155,7 +167,7 @@
     $(document).ready(function () {
         $('.ckeditor').ckeditor();
     });
-    CKEDITOR.config.height='400px';
+    CKEDITOR.config.height='100px';
     $(document).ready(function (e) {
         $('#gambar').change(function(){
                 let reader = new FileReader();
